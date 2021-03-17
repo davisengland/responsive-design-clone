102a1c76ad47bad345be6c4e9447b94bd0d3771c
davisengland <davisrodneyengland@gmail.com>
2021-03-17 17:00:52 -0600

    Still working on the header

---
+++ responsive-design-app/src/Components/Header.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+    constructor() {
+        super()
+        this.state = {
+            menu: 'closed'
+        }
+    }
+
+    showMenu = () => {
+        if(this.state.menu === 'closed') {
+            this.setState({menu: 'open'})
+        } else {
+            this.setState({menu: 'closed'})
+        }
+    }
+
+    render() {
+        return(
+            <div className='header-menu'>
+                <header>
+                    <h1>Start Bootstrap</h1>
+                    <button className='menu-button' onClick={() => this.showMenu()}>
+                        <h2>Menu</h2>
+                        <div className='menu-icon'>
+                            <div className='bar'></div>
+                            <div className='bar'></div>
+                            <div className='bar'></div>
+                        </div>
+                    </button>
+                </header>
+                <ul className={this.state.menu === 'closed' ? 'closed' : ''}>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        )
+    }
+}
